ff7417972b422edbc1c7ba6d078e0175db251fa0
seungyeonnn <tiger_0724@naver.com>
2023-10-24 14:41:57 +0900

    add redux

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,12 +4,21 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from '../node_modules/react-router-dom/dist/index';
+import { createStore } from 'redux';
+import rootReducer from './modules/index';
+import { composeWithDevTools } from '../node_modules/redux-devtools-extension/index';
+import { Provider } from '../node_modules/react-redux/es/exports';
+
+// 스토어 생성
+const store = createStore(rootReducer, composeWithDevTools());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
 );
 
 // If you want to start measuring performance in your app, pass a function
